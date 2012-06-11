af12e13a25c86389dd59181c359b9996b0a81b61
Ben Rosas <ballPointPenguin@gmx.us>
2012-06-10 20:06:06 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigsky::Application.config.secret_token = '5fd5d3dba1425404a8d232639f7d882e51d7a4c520a03f4544f8559f6ff1b7488314837d8ee4c40087df9b7a8f0a14a28edc36797683de9bee28270f64de31ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bigsky::Application.config.session_store :cookie_store, key: '_bigsky_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bigsky::Application.config.session_store :active_record_store
